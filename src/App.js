83f8b7b25968f45aa257f9f72cb1df01a24182c5
Md Amir Sohail <mdamirsohail1@gmail.com>
2023-02-09 19:31:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,21 @@
-import React,{useState} from "react";
-import Categories from "./components/Categories";
-import NavBar from "./components/NavBar";
-import ShowItems from "./components/ShowItems";
-import Slider from "./components/Slider";
-import appContext from "./context/appContext";
-import data from './data.json'
-import SignIn from "./SignnIn-SignUp/SignIn";
-import SignUp from "./SignnIn-SignUp/SignUp";
+import React from "react";
+import { Routes, Route, json } from 'react-router-dom'
+import AddToFav from "./components/AddToFav";
+import Home from "./components/Home";
+import './Custom.css'
 
 function App() {
-  const [dropdown, setDropdown] = useState(false);
-    const [cat , setCat] = useState(false);
-    const [subCat , setSubCat] = useState(false);
-    const [itemCat, setItemCat] = useState("");
-    const [itemSubCat, setItemSubCat] = useState("");
-  return ( 
-    <appContext.Provider  value={{
-      setDropdown:setDropdown,
-      setCat:setCat,
-      setSubCat:setSubCat,
-      setItemCat:setItemCat,
-      setItemSubCat:setItemSubCat,
-      data:data,
-      cat:cat,
-      subCat:subCat,
-      dropdown:dropdown,
-      itemCat:itemCat,
-      itemSubCat:itemSubCat
-    }}>
-      <div className="App">
-        <NavBar />
-        <Categories />
-        <Slider />
-        <ShowItems />
-        <SignIn/>
-        <SignUp/>
 
+   return (
+      <div className="App">
+         
+         <Routes>
+            <Route path="" element={<Home />}/>
+            <Route path="searchfav" element={<AddToFav />}/>
+            <Route path="editfav" element={<AddToFav/>}/>
+         </Routes>
       </div>
-    </appContext.Provider>
-  );
+   );
 }
 
 export default App;
